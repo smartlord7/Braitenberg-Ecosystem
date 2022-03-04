685d8b16c56a20745f1743c6e783e58649ee2ec1
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-03-04 10:21:43 +0000

    MASTER: Add base project.

---
+++ src/Braitenberg-Ecosystem/Assets/Scripts/CarDetectorScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class CarDetectorScript : MonoBehaviour {
+
+	public float angle = 360;
+	public bool ApplyThresholds, ApplyLimits;
+	public float MinX, MaxX, MinY, MaxY;
+	private bool useAngle = true;
+
+	public float output;
+	public int numObjects;
+
+	void Start()
+	{
+		output = 0;
+		numObjects = 0;
+
+		if (angle > 360)
+		{
+			useAngle = false;
+		}
+	}
+
+	void Update()
+	{
+		// YOUR CODE HERE
+
+		GameObject[] cars = null;
+		GameObject closestCar = null;
+
+		output = 0;
+
+
+	}
+
+	public virtual float GetOutput() { throw new NotImplementedException(); }
+
+	// Returns all "Light" tagged objects. The sensor angle is not taken into account.
+	GameObject[] GetAllCars()
+	{
+		return GameObject.FindGameObjectsWithTag("CarToFollow");
+	}
+
+	// YOUR CODE HERE
+
+
+}
