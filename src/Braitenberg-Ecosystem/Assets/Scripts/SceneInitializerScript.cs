ebbd1e4ab89ee2f7c975ef6da27cdbb042c6e86f
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-03-15 01:49:53 +0000

    MASTER: Remove unused script.

--- src/Braitenberg-Ecosystem/Assets/Scripts/SceneInitializerScript.cs
+++ src/Braitenberg-Ecosystem/Assets/Scripts/SceneInitializerScript.cs
@@ -35,7 +35,6 @@ public class SceneInitializerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         GenerateScene();
     }
