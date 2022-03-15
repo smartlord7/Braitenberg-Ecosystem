787ba5c9d248b0b72f7310eeb904c1d15355d2ba
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-03-15 01:57:25 +0000

    MASTER: Add final camera movement.

---
+++ src/Braitenberg-Ecosystem/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public float MainSpeed = 100.0f;
+    public float ShiftAdd = 250.0f;
+    public float MaxShift = 1000.0f;
+    public float CamSens = 0.25f;
+    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    private float totalRun = 1.0f;
+
+    void Update()
+    {
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * CamSens, lastMouse.x * CamSens, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+        
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+
+        if (p.sqrMagnitude > 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                totalRun += Time.deltaTime;
+                p = p * totalRun * ShiftAdd;
+                p.x = Mathf.Clamp(p.x, -MaxShift, MaxShift);
+                p.y = Mathf.Clamp(p.y, -MaxShift, MaxShift);
+                p.z = Mathf.Clamp(p.z, -MaxShift, MaxShift);
+            }
+            else
+            {
+                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+                p = p * MainSpeed;
+            }
+
+            p = p * Time.deltaTime;
+            Vector3 newPosition = transform.position;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(p);
+                newPosition.x = transform.position.x;
+                newPosition.z = transform.position.z;
+                transform.position = newPosition;
+            }
+            else
+            {
+                transform.Translate(p);
+            }
+        }
+    }
+
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+
+        return p_Velocity;
+    }
+}
